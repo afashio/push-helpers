6f9f314f20824d871b8aa579e4279b61909d408b
afashio <push.webstudio@gmail.com>
2020-07-20 11:06:47 +0300

    Inital commit

---
+++ src/traits/BasicStatusTrait.php
@@ -0,0 +1,62 @@
+<?php
+
+
+namespace common\traits;
+
+
+use common\interfaces\BasicStatusInterface;
+use Yii;
+use yii\base\InvalidArgumentException;
+
+/**
+ * Trait BasicStatusTrait
+ *
+ * @package common\traits
+ */
+trait BasicStatusTrait
+{
+
+    /**
+     * @return int
+     */
+    public static function getActiveStatus()
+    {
+        return BasicStatusInterface::STATUS_ACTIVE;
+    }
+
+    /**
+     * @return int
+     */
+    public static function getInActiveStatus()
+    {
+        return BasicStatusInterface::STATUS_INACTIVE;
+    }
+
+    /**
+     * @param $statusID
+     *
+     * @return string
+     * @throws \yii\base\InvalidArgumentException
+     */
+    public static function getStatus($statusID): string
+    {
+        $list = self::status_list();
+        if (isset($list[$statusID])) {
+            return $list[$statusID];
+        }
+
+        throw new InvalidArgumentException('Given status is not valid!');
+    }
+
+    /**
+     * @return array
+     */
+    public static function status_list()
+    {
+        return [
+            BasicStatusInterface::STATUS_INACTIVE => Yii::t('app', 'Не отображать'),
+            BasicStatusInterface::STATUS_ACTIVE => Yii::t('app', 'Отображать'),
+        ];
+    }
+
+}
---
+++ src/traits/ModelTranslationTrait.php
@@ -0,0 +1,65 @@
+<?php
+
+
+namespace common\traits;
+
+
+use common\models\UploadFile;
+use creocoder\translateable\TranslateableBehavior;
+use Yii;
+
+/**
+ * Trait ModelTranslationTrait
+ *
+ * @package common\traits
+ * @mixin TranslateableBehavior
+ */
+trait ModelTranslationTrait
+{
+
+    public function behaviors()
+    {
+        $behaviors = [
+            'translateable' => [
+                'class' => TranslateableBehavior::class,
+                'translationAttributes' => ['text'],
+                // translationRelation => 'translations',
+                'translationLanguageAttribute' => 'language',
+            ],
+        ];
+
+        return array_merge(parent::behaviors(), $behaviors);
+    }
+
+    public function saveModelWithImage($fileInput = 'imageFiles'): bool
+    {
+        if ($this->saveModel()) {
+            UploadFile::upload($this, true, true, $fileInput);
+
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * @return bool
+     * @throws \ReflectionException
+     */
+    public function saveModel(): bool
+    {
+         if ($this->load(Yii::$app->request->post())) {
+            $translations = Yii::$app->request->post((new \ReflectionClass($this))->getShortName() . 'Lang', []);
+            foreach ($translations as $language => $data) {
+                foreach ($data as $attribute => $value) {
+                    $this->translate($language)->$attribute = $value;
+                }
+            }
+
+            return $this->save(false);
+        }
+
+        return false;
+    }
+
+}
